5be3b162ed84bd9c27c496e02063dd3981a5dfa2
bol0bal4 <vunhan93@gmail.com>
2016-02-19 09:33:35 +0700

    1st commit

---
+++ poiterValue.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  poiterValue.c
+ *
+ *    Description:  First Pointer example
+ *
+ *        Version:  1.0
+ *        Created:  02/19/2016 09:31:20 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int pv = 1;
+	int* pi = &pv;
+	printf("%s\n", pi);
+	return 0;
+}
