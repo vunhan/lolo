b33aa02b0d715d181fd4c74c3e9bfd84bb6a6056
bol0bal4 <vunhan93@gmail.com>
2016-02-19 10:50:37 +0700

    Add new file code

---
+++ helloworld.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.c
+ *
+ *    Description:  Hello World
+ *
+ *        Version:  1.0
+ *        Created:  02/19/2016 10:47:26 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int int main(int argc, char *argv[])
+{
+	printf("Hello");
+	return 0;
+}
